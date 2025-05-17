b78384a2f8054c5023da98765a68065c8dfe7e70
Andrey Baryshev <andrey.bar66@gmail.com>
2025-05-18 02:49:02 +0500

    Edit return type data

--- services/security.py
+++ services/security.py
@@ -4,6 +4,7 @@ import cv2
 import segno
 from qreader import QReader
 
+from models.Request import Request, Status
 from services.database import DataBase as db
 
 
@@ -11,7 +12,7 @@ class Security:
     PATH_QRCodes = "../QRCodes/"
 
     @staticmethod
-    def create_qr(user_id: int = 10) -> str:
+    def create_qr(user_id: int = 10) -> Request:
         """
         Создаёт QR-код на основе идентификатора пользователя и сохраняет его в виде изображения.
 
@@ -25,17 +26,16 @@ class Security:
         path = Security.PATH_QRCodes + f"{user_id}.png"
         os.makedirs(Security.PATH_QRCodes, exist_ok=True)
         qrcode.save(path)
-        return path
+        return Request(status=Status.OK, value=path)
 
     @staticmethod
-    def check_qr(file_name: str, event_id: int) -> bool:
+    def check_qr(file_name: str, workshop_id: str) -> Request:
         """
         Проверить QR код
 
         Args:
             file_name: Название файла с QR кодом
-
-            event_id: ID воркшопа
+            workshop_id: ID воркшопа
 
         Returns:
             True, если пользователь зарегистрирован на данное мероприятие, иначе False
@@ -47,9 +47,8 @@ class Security:
             image = cv2.imread(path)
             decoded_texts: tuple = qreader.detect_and_decode(image=image)
             user_id = int(decoded_texts[0])
-            # TODO: Добавить более понятную ошибку, почему False
-        except Exception:
-            return False
+        except Exception as err:
+            return Request(status=Status.ERROR, message=f"QR code decoding error: {err}")
 
-        workshop = db.get_workshop(event_id)
-        return workshop.registered_user_ids.get[user_id]
+        workshop = db.get_workshop(workshop_id)
+        return Request(status=Status.OK, value=workshop.registered_user_ids.get[user_id])

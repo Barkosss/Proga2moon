630a5d54feb0bba8f5fbd802f524508852f2252b
Andrey Baryshev <andrey.bar66@gmail.com>
2025-05-18 02:36:32 +0500

    Support model Event & edit comment

--- services/database.py
+++ services/database.py
@@ -4,9 +4,7 @@ from datetime import datetime
 from enum import Enum
 from pathlib import Path
 
-from models.Request import Request, Status
-from models.User import User
-from models.Workshop import Workshop
+from models import *
 
 
 class FileData(Enum):
@@ -16,13 +14,16 @@ class FileData(Enum):
 
     Attributes:
         USERS (str): Файл для хранения данных о пользователях.
-
         WORKSHOPS (str): Файл для хранения информации о воркшопах.
+        EVENTS (str): Файл для хранения информации об ивентах
     """
     USERS = "users.json"
     WORKSHOPS = "workshops.json"
+    EVENTS = "event.json"
+    QUESTIONS = "questions.json"
 
 
+# TODO: Подумать и добавить работу с таблицей вопросов
 class DataBase:
     """
     Класс для работы с локальной JSON-базой данных, хранящей информацию о пользователях и воркшопах.
@@ -62,13 +63,12 @@ class DataBase:
     def has_user(self, user_id: int) -> Request:
         """
         Проверить наличие пользователя среди зарегистрированных
-
         """
         try:
             self._read_data(FileData.USERS)
             return Request(status=Status.OK, value=self.data[user_id])
         except KeyError as err:
-            return Request(status=Status.ERROR, message="...", value=False)
+            return Request(status=Status.ERROR, message=f"User by id({user_id}) not found: {err}", value=False)
 
     def get_user(self, user_id: int) -> Request:
         """
@@ -97,13 +97,12 @@ class DataBase:
         except KeyError as err:
             return Request(status=Status.ERROR, message=f"Couldn't add a new user: {err}")
 
-    def get_workshop(self, event_id: int, workshop_id: int) -> Request:
+    def get_workshop(self, workshop_id: str) -> Request:
         """
         Получить воркшоп по ID
 
         Args:
-            :param event_id: Идентификатор мероприятия
-            :param workshop_id: Идентификатор воркщопа для получения
+            :param workshop_id: Идентификатор воркшопа для получения
         """
         self._read_data(FileData.WORKSHOPS)
         try:
@@ -115,44 +114,112 @@ class DataBase:
         """
         Получить список воркшопов
 
+        Args:
+            event_id: Идентификатор ивента
         """
-        self._read_data(FileData.WORKSHOPS)
+        self._read_data(FileData.EVENTS)
+
+        if self.data.get(event_id) is None:
+            return Request(status=Status.ERROR, message=f"An event by id({event_id}) is not found", value=False)
+
+        event: Event = self.data[event_id]
         workshops: list[Workshop] = []
 
         try:
-            for workshop_id in self.data[FileData.WORKSHOPS].keys():
+            for workshop_id in event.workshop_ids:
                 workshop_request: Request = self.get_workshop(workshop_id)
                 if workshop_request.status == Status.OK:
                     workshops.append(workshop_request.value)
 
             return Request(status=Status.OK, value=workshops)
         except KeyError as err:
-            return Request(status=Status.ERROR, message=f"Couldn't get all the workshops: {err}", value=False)
+            return Request(status=Status.ERROR,
+                           message=f"Couldn't get all the workshops from event ({event_id}): {err}", value=False)
 
-    def add_workshop(self, workshop: Workshop) -> Request:
+    def add_workshop(self, event_id: int, workshop: Workshop) -> Request:
         """
         Добавить новый воркшоп
 
         Args:
+            event_id: Идентификатор ивента
             workshop: Объект вокршопа для добавления
         """
+        self._read_data(FileData.EVENTS)
+
+        if not (event_id in self.data.keys()):
+            return Request(status=Status.ERROR, message="Event id is not found", value=False)
+
+        event: Event = Event.from_dict(self.data[event_id])
+        event.workshop_ids.append(workshop.id)
+
         try:
-            self.data[workshop.id] = workshop.to_dict()
+            self.data[event.id] = event.to_dict()
+            self._write_data(FileData.EVENTS)
+            return Request(status=Status.OK)
+        except KeyError as err:
+            return Request(status=Status.ERROR,
+                           message=f"Couldn't add a new workshop ({workshop.id}) to event ({event_id}): {err}",
+                           value=False)
+
+    def get_event(self, event_id: int) -> Request:
+        """
+        Получить ивент
+
+        Args:
+            event_id: Идентификатор ивента
+        """
+        self._read_data(FileData.EVENTS)
+
+        if self.data.get(event_id) is None:
+            return Request(status=Status.ERROR, message=f"An event by id({event_id}) is not found", value=False)
+
+        event: Event = self.data[event_id]
+        return Request(status=Status.OK, value=event.to_dict())
+
+    def get_events(self) -> Request:
+        """
+        Получить ивенты
+        """
+        self._read_data(FileData.EVENTS)
+
+        events: list[Event] = []
+        for event_id in self.data.keys():
+            events.append(self.data[event_id].to_dict())
+
+        return Request(status=Status.OK, value=events)
+
+    def add_event(self, event: Event, workshops: list[Workshop]) -> Request:
+        """
+        Добавить новый ивент
+
+        Args:
+            event: Объект ивента
+            workshops: Список воркшопов
+        """
+        try:
+            self._read_data(FileData.EVENTS)
+
+            if not (self.data.get(event.id) is None):
+                return Request(status=Status.ERROR, message=f"An event by id({event.id}) is not found", value=False)
+
+            for workshop in workshops:
+                if workshop.id not in event.workshop_ids:
+                    event.workshop_ids.append(workshop.id)
+
+            self._write_data(FileData.EVENTS)
+        except KeyError as err:
+            return Request(status=Status.ERROR, message=f"An event by id({event.id}) or workshop is not found: {err}")
+
+        try:
+            self._read_data(FileData.WORKSHOPS)
+
+            for workshop in workshops:
+                self.data[workshop.id] = workshop.to_dict()
+
             self._write_data(FileData.WORKSHOPS)
             return Request(status=Status.OK)
         except KeyError as err:
-            return Request(status=Status.ERROR, message=f"Couldn't add a new workshop: {err}", value=False)
-
-    def get_event(self, event_id: int) -> Request:
-        pass
-
-    def get_events(self) -> Request:
-        pass
-
-    """
-    def add_event(self, event: Event) -> Request:
-        pass
-    """
+            return Request(status=Status.ERROR, message=f"A workshop is not found: {err}")
 
     @staticmethod
     def create_backup():

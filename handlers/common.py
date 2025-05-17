3936b37dae45a2fbacf6be6009630eb013c14e13
Barkosss <andrey.bar66@gmail.com>
2025-05-17 12:37:00 +0500

    Edit comments

--- handlers/common.py
+++ handlers/common.py
@@ -3,6 +3,6 @@ from utils.decorators import user_only
 def register_handlers(bot):
 
     @bot.message_handler(commands=["start", "help"])
-    # @user_only - доступен только зареганным пользователям
+    # @user_only - доступен только зарегистрированным пользователям
     def handle_start(message):
         bot.reply_to(message, "Welcome!")

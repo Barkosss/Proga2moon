43c373b43b9fe593f9f9bc7f9ac17f16acc6c5fa
Andrey Baryshev <andrey.bar66@gmail.com>
2025-05-17 23:51:41 +0500

    Add new import

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,12 @@
 __all__ = [
+    'Request',
+    'Status',
     'User',
     'Workshop',
     'Event'
 ]
 
+from .Request import Request, Status
 from .User import User
 from .Workshop import Workshop
 from .Event import Event

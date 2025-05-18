e4f5482e1cdae373ca1a4a3e1573c73b265fe8c8
Andrey Baryshev <77407266+Barkosss@users.noreply.github.com>
2025-05-18 18:23:41 +0500

    Update dev-database (#8)
    * add test funcs
    
    * Add поле event_id
    
    * Remove list admins & Add Request model (#5)
    
    * Create Request model & Edit return type data & edit docs
    
    * Add new requirements
    
    * Remove list of admins
    
    * Add new methods for event
    
    * Removed an extra field
    
    * Checking for an administrator in a specific event
    
    * В Workshop сделал uuid
    Добавил Event
    Сделал __init__.py))))))))
    
    * Deleting unnecessary files
    
    * Create new table
    
    * Add new import
    
    * Create new table: Questions
    
    * Support model Event & edit comment
    
    * Edit return type data
    
    * Update Request & add Enum dir
    
    * Create validators
    
    * Добавил ссылку на чат и метод обновления (добавления) и метод удаления ссылки
    
    * Добавил is_notified
    
    * Добавил speakers
    
    * Добавил admin_event_ids
    
    * Update README (#7)
    
    ---------
    
    Co-authored-by: orelkrylatiy <maxim700a@gmail.com>
    Co-authored-by: Konstantin Kolesnikov <kolesnikov.kons@gmail.com>

--- models/Workshop.py
+++ models/Workshop.py
@@ -1,6 +1,7 @@
+import uuid
 from dataclasses import dataclass, field, asdict
 from datetime import datetime
-from typing import List, Optional, Union
+from typing import List, Optional
 
 
 @dataclass
@@ -8,36 +9,48 @@ class Workshop:
     """
     Сущность мастер-класса (Event):
 
-    - id: UUID или целочисленный счётчик
+    - id: UUID
+    - event_id: UUID
     - title: название мероприятия
     - description: описание
-    - start: время начала (ISO-строка → datetime)
+    - location: место проведения (аудитория, комната, зал...)
+    - start: время начала
     - end: время окончания
     - registration_required: нужна ли регистрация
+    - speakers: список Telegram ID спикеров на мастер-классе
     - limited_capacity: ограничено ли число мест
     - capacity: число мест (если limited_capacity=True)
     - registered_user_ids: список ID зареганных участников
     - waiting_user_ids: список ID участников очереди
+    - is_notified: флаг, который указывает, начался ли мастер-класс => сделали ли уведомления о начале
     """
-    id: int
+    id: str
+    event_id: int
     title: str
     description: str
+    location: str
     start: datetime
     end: datetime
     registration_required: bool
     limited_capacity: bool
+    is_notified: bool
+    speakers: List[int]
     capacity: Optional[int] = None
-    registered_user_ids: List[Union[int, str]] = field(default_factory=list)
-    waiting_user_ids: List[Union[int, str]] = field(default_factory=list)
+    registered_user_ids: List[int] = field(default_factory=list)
+    waiting_user_ids: List[int] = field(default_factory=list)
 
     @classmethod
     def from_dict(cls, data: dict) -> "Workshop":
         return cls(
-            id=data["id"],
+            id=str(uuid.uuid4()),
+            event_id=data["event_id"],
             title=data["title"],
             description=data.get("description", ""),
+            location=data.get("location", ""),
             start=datetime.fromisoformat(data["start"]),
             end=datetime.fromisoformat(data["end"]),
+            speakers=data.get("speakers", []),
+            is_notified=data["is_notified"],
             registration_required=bool(data.get("registration_required", False)),
             limited_capacity=bool(data.get("limited_capacity", False)),
             capacity=data.get("capacity"),
